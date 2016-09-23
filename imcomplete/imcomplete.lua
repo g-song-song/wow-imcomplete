7e06475b08f54017cd1e99c865ab146a9fb58170
Seokseong Jeon <Seokseong Jeon>
2016-09-23 18:05:09 +0900

    prepare for locale specific

--- imcomplete/imcomplete.lua
+++ imcomplete/imcomplete.lua
@@ -1,9 +1,22 @@
 local frame, events = CreateFrame("Frame"), {};
+if GetLocale() == "koKR" then
+    q = {"이제 난 완전히 졌다!",
+         "이제 난 완전해졌다!",
+         "만 년 동안 응어리진 증오를 보여주마!",
+         "너흰 아직 준비가 안 됐다!",
+         "이제 너희는 준비가 돼-따!"}
+else
+    q = {"Now, I am complete!",
+         "Feel the hatred of ten thousand years!",
+         "You are not prepared!",
+         "Now, you are prepared!"}
+end
+l = table.getn(q)
 activated = false
 function events:UNIT_AURA(...)
     found = false
     for i = 1, 40 do
-        name, _, _, _, _, d, e, _, _, _, spellid = UnitAura("player", i)
+        _, _, _, _, _, _, _, _, _, _, spellid = UnitAura("player", i)
         -- dps: 162264
         -- tank: 187827
         if spellid == 162264 or spellid == 187827 then
@@ -11,18 +24,10 @@ function events:UNIT_AURA(...)
             break
         end
     end
-
     if found then
         if activated == false then
             activated = true
-            r = e - GetTime()
-            q = {"이제 난 완전히 졌다!",
-                 "이제 난 완전해졌다!",
-                 "만 년 동안 응어리진 증오를 보여주마!",
-                 "너흰 아직 준비가 안 됐다!",
-                 "이제 너희는 준비가 돼-따!"}
-            i = random(5)
-            SendChatMessage(q[i])
+            SendChatMessage(q[random(l)], "SAY")
         end
     else
         activated = false
