a267ce47dcb3647e486780d3769a4753c40b9073
Seokseong Jeon <Seokseong Jeon>
2016-10-15 22:55:38 +0900

    add quotes

--- imcomplete/imcomplete.lua
+++ imcomplete/imcomplete.lua
@@ -1,15 +1,29 @@
 local frame, events = CreateFrame("Frame"), {};
 if GetLocale() == "koKR" then
-    q = {"이제 난 완전히 졌다!",
-         "이제 난 완전해졌다!",
-         "만 년 동안 응어리진 증오를 보여주마!",
-         "너흰 아직 준비가 안 됐다!",
-         "이제 너희는 준비가 돼-따!"}
+    q = {
+      "이제 난 완전히 졌다!",
+      "이제 난 완전해졌다!",
+      "만 년 동안 응어리진 증오를 보여주마!",
+      "너흰 아직 준비가 안 됐다!",
+      "이제 너희는 준비가 돼-따!",
+      "나의 증오는 끝이 없다!",
+      "복수는 나의 것이다!",
+      "나에겐 오직 증오뿐!",
+      "아지노스의 칼날 맛을 봐라!",
+      "아지노스의 화염으로 불살라주마!"
+    }
 else
-    q = {"Now, I am complete!",
-         "Feel the hatred of ten thousand years!",
-         "You are not prepared!",
-         "Now, you are prepared!"}
+    q = {
+      "Now, I am complete!",
+      "Feel the hatred of ten thousand years!",
+      "You are not prepared!",
+      "Now, you are prepared!",
+      "My hatred is unending!",
+      "Vengeance is mine!",
+      "I feed only hatred!",
+      "Taste the blade of Azzinoth!",
+      "Burn with the flames of Azzinoth!"
+    }
 end
 l = table.getn(q)
 activated = false
